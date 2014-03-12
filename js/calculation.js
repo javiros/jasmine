036d2315dd2de68e89cba06f38afa8a99160be02
javiros <javier_rossetti1972@hotmail.com>
2014-03-12 19:49:44 +0000

    new push to heroku

--- js/calculation.js
+++ js/calculation.js
@@ -1,13 +1,39 @@
+//window.onload = function(){
+//    function addnumbers(){
+//        numberOne = parseInt(document.getElementById('number1'));
+//        numberTwo = parseInt(document.getElementById('number2'));
+//        var add = numberOne + numberTwo
+//        var element = document.getElementById('result');
+//        element.innerHTML = add;
+//
+//    }
+//
+//    addnumbers();
+//
+//}
 window.onload = function(){
-    function addnumbers(){
-        numberOne = parseInt(document.getElementById('number1'));
-        numberTwo = parseInt(document.getElementById('number2'));
-        var add = numberOne + numberTwo
-        var element = document.getElementById('result');
-        element.innerHTML = add;
+    var Converter = (function() {
 
-    }
+        // Private stuff up here
+        var converterValue = 2.2;
 
-    addnumbers();
 
+        // Public methods here
+        return {
+            convertFromPoundsToKIlo: function(weight) {
+                var num = parseInt(weight);
+
+                if (isNaN(num) == true) {
+                    throw new Error ("Not a number");
+                }
+                num = num / converterValue;
+                return Math.round(num);
+            },
+
+            convertFromMetricToImperial: function(weight) {
+                return weight / converterValue;
+            }
+
+        }
+    }())
 }
